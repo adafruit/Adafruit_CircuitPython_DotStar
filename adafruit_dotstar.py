367f6f40ca430d79f253bfc969f3544cff5c6afd
mcscope <mcscope@gmail.com>
2018-05-16 14:42:45 -0700

    Add persistent second buffer

--- adafruit_dotstar.py
+++ adafruit_dotstar.py
@@ -108,6 +108,7 @@ class DotStar:
         # 0xff bytes at the end.
         for i in range(self.end_header_index, len(self._buf)):
             self._buf[i] = 0xff
+        self._truebuf = [(255,255,255) for _ in range(n)]
         self._brightness = 1.0
         # Set auto_write to False temporarily so brightness setter does _not_
         # call show() while in __init__.
@@ -168,6 +169,7 @@ class DotStar:
         # LED startframe is three "1" bits, followed by 5 brightness bits
         # then 8 bits for each of R, G, and B. The order of those 3 are configurable and
         # vary based on hardware
+        self._truebuf = rgb
         if self.brightness < 1.0:
             rgb = [int(val * self._brightness) for val in rgb]
 
@@ -218,19 +220,16 @@ class DotStar:
 
     @brightness.setter
     def brightness(self, brightness):
-        brightness = min(max(brightness, 0.0), 1.0)
-        # scaling factor - new brightness / old.
-        # ex: going from 0.2 to 1 would be 1/0.2, so 5.0 * each color value
-        correction_factor = brightness / self.brightness
-        self._brightness = brightness
+        self._brightness = min(max(brightness, 0.0), 1.0)
 
-        if correction_factor != 1.0:
-            # We got a new global brightness, update all pixels in _buf
-            for i in range(START_HEADER_SIZE, self.end_header_index):
-                # only color values, not headers
-                if i % 4 != 0:
-                    new_val = int(self._buf[i] * correction_factor)
-                    self._buf[i] = min(max(new_val, 0), 255)
+        # We got a new global brightness, update all pixels in _buf
+        for index, rgb in enumerate(self._truebuf):
+            offset = index * 4 + START_HEADER_SIZE
+            rgb = [int(val * self._brightness) for val in rgb]
+
+            self._buf[offset + 1] = rgb[self.pixel_order[0]]
+            self._buf[offset + 2] = rgb[self.pixel_order[1]]
+            self._buf[offset + 3] = rgb[self.pixel_order[2]]
 
         if self.auto_write:
             self.show()
